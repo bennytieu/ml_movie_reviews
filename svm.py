f4d2e792b982df4d7794ccbb6f3c73869d6611c3
bonecracker <benny.tieu@gmail.com>
2016-04-15 19:27:59 +0800

    Added lemmanization, 0.11 better!

--- svm.py
+++ svm.py
@@ -2,96 +2,117 @@
 from __future__ import print_function
 
 import time
-import json
 import sys
 import re
 
 import pandas as pd 
 import numpy as np
 import string
+import nltk
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import text
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.multiclass import OneVsOneClassifier
+from nltk import word_tokenize          
+from nltk.stem.porter import PorterStemmer
 
 startTime = time.time()
 currentTime = startTime
 
+
+
 def main():
 	global currentTime
-	exclude = set(string.punctuation)
 
-	with open("settings.json", "r") as jsonFile:
-		jsonData = json.load(jsonFile)
+	pathTrainingData = "data/train.tsv"
+	pathTestData = "data/test.tsv"
 
-		pathTrainingData = jsonData["train_path"]
-		pathTestData = jsonData["test_path"]
+	print("\t{: <40}".format("Importing training data."), end="")
+	sys.stdout.flush()
 
-		print("\t{: <40}".format("Importing training data."), end="")
+	train = pd.read_csv(pathTrainingData, 
+						header=0, 
+						delimiter="\t",
+						quoting = 3
+						)
+	printTime(currentTime)
+
+	text_clf = Pipeline([('vect', CountVectorizer(
+									analyzer = "word",
+									tokenizer = tokenize,
+									preprocessor = None,
+									#stop_words = {text.ENGLISH_STOP_WORDS},
+									lowercase = True,
+									max_df = 1.0,
+									min_df = 0.000040,
+									ngram_range=(1, 2)
+									)),
+						('tfidf', TfidfTransformer()),
+						('lSVC', OneVsOneClassifier(LinearSVC(C=0.32)))])	
+
+	print("\t{: <40}".format("Fitting training data."), end="")
+	sys.stdout.flush()
+
+	phrases = []
+	for x in xrange(0,len(train['Phrase'])):
+		phrases.append(train['Phrase'][x])
+
+	text_clf = text_clf.fit(phrases, train['Sentiment'])
+	printTime(currentTime)
+
+	print("\t{: <40}".format("Importing test data."), end="")
+	sys.stdout.flush()
+
+	test = pd.read_csv(	pathTestData,
+						header=0,
+						delimiter="\t",
+						quoting = 3
+						)
+	printTime(currentTime)
+
+	print("\t{: <40}".format("Making predictions for test."), end="")
+	sys.stdout.flush()
+
+	phraseTest = []
+	for x in xrange(0,len(test['Phrase'])):
+		phraseTest.append(test['Phrase'][x])
+
+	predictedData = text_clf.predict(phraseTest)
+	printTime(currentTime)
+
+	with open("submission.csv", "w") as submissionFile:
+		submissionFile.write("PhraseID,Sentiment\n")
+
+		print("\t{: <40}".format("Creating submission file"), end="")
 		sys.stdout.flush()
-
-		train = pd.read_csv(pathTrainingData, 
-							header=0, 
-							delimiter="\t",
-							quoting = 3
-							)
-		printTime(currentTime)
-
-		text_clf = Pipeline([('vect', CountVectorizer(
-										analyzer = "word",
-										tokenizer = None,
-										preprocessor = None,
-										stop_words = {text.ENGLISH_STOP_WORDS},
-										lowercase = True,
-										#max_df = 1.0,
-										min_df = 0.000030,
-										ngram_range=(1, 2)
-										)),
-							('tfidf', TfidfTransformer()),
-							('lSVC', SVC(kernel='linear'))])	
-
-		print("\t{: <40}".format("Fitting training data."), end="")
-		sys.stdout.flush()
-
-		phrases = []
-		sentiments = []
-		for x in xrange(0,len(train['Phrase'])):
-			phrases.append(''.join(ch for ch in train['Phrase'][x] if ch not in exclude))
-
-		text_clf = text_clf.fit(phrases, train['Sentiment'])
-		printTime(currentTime)
-
-		print("\t{: <40}".format("Importing test data."), end="")
-		sys.stdout.flush()
-
-		test = pd.read_csv(	pathTestData,
-							header=0,
-							delimiter="\t",
-							quoting = 3
-							)
-		printTime(currentTime)
-
-		print("\t{: <40}".format("Making predictions for test."), end="")
-		sys.stdout.flush()
-
-		phraseTest = []
 		for x in xrange(0,len(test['Phrase'])):
-			phraseTest.append(''.join(ch for ch in test['Phrase'][x] if ch not in exclude))
-
-		predictedData = text_clf.predict(phraseTest)
+			submissionFile.write(str(test['PhraseId'][x]) + "," + str(predictedData[x])+"\n")
 		printTime(currentTime)
 
-		with open("submission.csv", "w") as submissionFile:
-			submissionFile.write("PhraseID,Sentiment\n")
+def tokenize(text):
+    text = "".join([ch for ch in text if ch not in string.punctuation])
+    tokens = nltk.word_tokenize(text)
+    res = stemTokens(tokens)
+    #res = lemLokens(tokens)
 
-			print("\t{: <40}".format("Creating submission file"), end="")
-			sys.stdout.flush()
-			for x in xrange(0,len(test['Phrase'])):
-				submissionFile.write(str(test['PhraseId'][x]) + "," + str(predictedData[x])+"\n")
-			printTime(currentTime)
+    return res
+
+def stemTokens(tokens):
+	stemmer = PorterStemmer()
+	stemmed = []
+	for token in tokens:
+		stemmed.append(stemmer.stem(token))
+	return stemmed
+
+def lemLokens(tokens):
+	lemmer = nltk.stem.WordNetLemmatizer()
+	lemmed = []
+	for token in tokens:
+		stemmed.append(lemmer.lemmatize(token))
+	return lemmed
 
 def printTime(t0):
 	global currentTime
